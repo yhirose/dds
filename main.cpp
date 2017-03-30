fd96e649e3904941f8fadb9b50c61973f9fe9d32
yhirose <yuji.hirose.bug@gmail.com>
2017-03-29 23:32:18 -0400

    数字のパース

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include "linenoise.hpp"
+#include "peglib.h"
 using namespace std;
 
+typedef shared_ptr<peg::Ast> AST;
+
+const auto grammar = R"(
+  NUMBER <- '-'? [0-9]+ ('.' [0-9]+)?
+)";
+
 int main() {
+  // パーサーの生成
+  peg::parser parser(grammar);
+  assert(parser);
+
+  // 抽象構文木(AST)生成機能を使用する
+  parser.enable_ast();
+
   // REPL
   while (true) {
     // 行の入力
@@ -13,10 +27,18 @@ int main() {
       break;
     }
 
-    // 入力された文字列の表示
-    cout << line << endl;
+    // 入力行をパースし、ASTを生成
+    AST ast;
+    auto ret = parser.parse(line.c_str(), ast);
 
-    // 入力された行を履歴に追加
-    linenoise::AddHistory(line.c_str());
+    if (ret) {
+      // ASTの表示
+      cout << peg::ast_to_s(ast);
+
+      // 入力された行を履歴に追加
+      linenoise::AddHistory(line.c_str());
+    } else {
+      cout << "syntax error..." << endl;
+    }
   }
 }
